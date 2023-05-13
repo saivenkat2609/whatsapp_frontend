1c3e2452e876b0232d8abc766fa831d19b848824
saivenkat2609 <112820183+saivenkat2609@users.noreply.github.com>
2023-05-13 19:17:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import "./style.css";
+import SearchIcon from '@mui/icons-material/Search';
+import GroupsIcon from '@mui/icons-material/Groups';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import FilterTiltShiftIcon from '@mui/icons-material/FilterTiltShift';
+import MarkUnreadChatAltIcon from '@mui/icons-material/MarkUnreadChatAlt';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
+function App(){
+  return (
+    <div className="sect">
+      <div className="leftbar">
+        <div className="topbar">
+          <div>
+            <img className="prodim" src="https://pps.whatsapp.net/v/t61.24694-24/322277624_103272085996774_8552160575219247208_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQdAZDinogCDhEMuRBNUQ07a3cHUJHpEKcf72LuSDnKew&oe=644CF340" alt="not found"></img>
+          </div>
+          <div className="comdim">
+            <GroupsIcon></GroupsIcon>
+          </div>
+          <div className="stadim"><FilterTiltShiftIcon></FilterTiltShiftIcon></div>
+          <div className="notdim"><MarkUnreadChatAltIcon></MarkUnreadChatAltIcon></div>
+          <div className="mendim"><MoreVertIcon></MoreVertIcon></div>
+        </div>
+        <div className="searchbox">
+          <div className="searchtxt">
+            <SearchIcon></SearchIcon>
+            <div><input className="searchcon" type="text" placeholder="Search or start new chat"></input></div>
+          </div>
+          <div className="fildim"><FilterListIcon></FilterListIcon></div>
+        </div>
+        <div className="chatlist">
+          <span><img className="chatpro" src="https://pps.whatsapp.net/v/t61.24694-24/322277624_103272085996774_8552160575219247208_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQdAZDinogCDhEMuRBNUQ07a3cHUJHpEKcf72LuSDnKew&oe=644CF340"></img></span>
+          <span className="chatinfo">
+            <span className="conname">Venkat</span>
+            <span className="conmsg">hi</span>
+          </span>
+          <span className="time">10:00PM</span>
+        </div>
+        <div className="chatlist">
+          <span><img className="chatpro" src="https://pps.whatsapp.net/v/t61.24694-24/322277624_103272085996774_8552160575219247208_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQdAZDinogCDhEMuRBNUQ07a3cHUJHpEKcf72LuSDnKew&oe=644CF340"></img></span>
+          <span className="chatinfo">
+            <span className="conname">Venkat</span>
+            <span className="conmsg">hi</span>
+          </span>
+          <span className="time">10:00PM</span>
+        </div>
+        <div className="chatlist">
+          <span><img className="chatpro" src="https://pps.whatsapp.net/v/t61.24694-24/322277624_103272085996774_8552160575219247208_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQdAZDinogCDhEMuRBNUQ07a3cHUJHpEKcf72LuSDnKew&oe=644CF340"></img></span>
+          <span className="chatinfo">
+            <span className="conname">Venkat</span>
+            <span className="conmsg">hi</span>
+          </span>
+          <span className="time">10:00PM</span>
+        </div>
+        <div className="chatlist">
+          <span><img className="chatpro" src="https://pps.whatsapp.net/v/t61.24694-24/322277624_103272085996774_8552160575219247208_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQdAZDinogCDhEMuRBNUQ07a3cHUJHpEKcf72LuSDnKew&oe=644CF340"></img></span>
+          <span className="chatinfo">
+            <span className="conname">Venkat</span>
+            <span className="conmsg">hi</span>
+          </span>
+          <span className="time">10:00PM</span>
+        </div>
+        <div className="chatlist">
+          <span><img className="chatpro" src="https://pps.whatsapp.net/v/t61.24694-24/322277624_103272085996774_8552160575219247208_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQdAZDinogCDhEMuRBNUQ07a3cHUJHpEKcf72LuSDnKew&oe=644CF340"></img></span>
+          <span className="chatinfo">
+            <span className="conname">Venkat</span>
+            <span className="conmsg">hi</span>
+          </span>
+          <span className="time">10:00PM</span>
+        </div>
+        <div className="chatlist">
+          <span><img className="chatpro" src="https://pps.whatsapp.net/v/t61.24694-24/322277624_103272085996774_8552160575219247208_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQdAZDinogCDhEMuRBNUQ07a3cHUJHpEKcf72LuSDnKew&oe=644CF340"></img></span>
+          <span className="chatinfo">
+            <span className="conname">Venkat</span>
+            <span className="conmsg">hi</span>
+          </span>
+          <span className="time">10:00PM</span>
+        </div>
+        <div className="chatlist">
+          <span><img className="chatpro" src="https://pps.whatsapp.net/v/t61.24694-24/322277624_103272085996774_8552160575219247208_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQdAZDinogCDhEMuRBNUQ07a3cHUJHpEKcf72LuSDnKew&oe=644CF340"></img></span>
+          <span className="chatinfo">
+            <span className="conname">Venkat</span>
+            <span className="conmsg">hi</span>
+          </span>
+          <span className="time">10:00PM</span>
+        </div>
+        <div className="chatlist">
+          <span><img className="chatpro" src="https://pps.whatsapp.net/v/t61.24694-24/322277624_103272085996774_8552160575219247208_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQdAZDinogCDhEMuRBNUQ07a3cHUJHpEKcf72LuSDnKew&oe=644CF340"></img></span>
+          <span className="chatinfo">
+            <span className="conname">Venkat</span>
+            <span className="conmsg">hi</span>
+          </span>
+          <span className="time">10:00PM</span>
+        </div>
+        <div className="chatlist">
+          <span><img className="chatpro" src="https://pps.whatsapp.net/v/t61.24694-24/322277624_103272085996774_8552160575219247208_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQdAZDinogCDhEMuRBNUQ07a3cHUJHpEKcf72LuSDnKew&oe=644CF340"></img></span>
+          <span className="chatinfo">
+            <span className="conname">Venkat</span>
+            <span className="conmsg">hi</span>
+          </span>
+          <span className="time">10:00PM</span>
+        </div>
+      </div>
+      <div className="rightbar">
+        <div className="about">
+          <div>
+            <img className="prodim" src="https://pps.whatsapp.net/v/t61.24694-24/322277624_103272085996774_8552160575219247208_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQdAZDinogCDhEMuRBNUQ07a3cHUJHpEKcf72LuSDnKew&oe=644CF340"></img>
+          </div>
+          <div className="abtname">Venkat</div>
+          <div className="chatsrc"><SearchIcon></SearchIcon></div>
+          <div className="chatmenu"><MoreVertIcon></MoreVertIcon></div>
+        </div>
+        <div className='msgright'>
+          
+        </div>
+        <div className="send">
+          <InsertEmoticonIcon className="emoji"></InsertEmoticonIcon>
+          <AttachFileIcon className="file"></AttachFileIcon>
+          <div className="searchtxt1">
+            <div><input className="searchcon1" type="text" placeholder="Type a Message"></input></div>
+          </div>
+          <div className="aboutspeak">
+            <KeyboardVoiceIcon></KeyboardVoiceIcon>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+    
+  )
+}
+export default App;
